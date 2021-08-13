a3df88a5b65c1c38ed7742099d18ce2fe7105e6b
Josh Guilfoyle <jasta@devtcg.org>
2021-08-13 15:38:54 -0700

    Major updates to use natural motor resistance, fix move column bugs, integrate real sorting, etc

--- shuffl3bot.py
+++ shuffl3bot.py
@@ -9,7 +9,11 @@ from time import sleep
 from ev3dev2.motor import MediumMotor, Motor, OUTPUT_A, OUTPUT_D, SpeedDPS, SpeedPercent, SpeedValue
 from ev3dev2.button import Button
 from ev3dev2.sound import Sound
+from ev3dev2.led import Leds
+from ev3dev2.button import Button
+from ev3dev2.console import Console
 import ev3dev2
+from moves_generator import ShuffleSolver
 
 def debug_print(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
@@ -39,19 +43,23 @@ class ArmStateDesc:
 
 # Implements the specific geometry of the shuffle bot, with all its weird mechanical quirks :)
 class ShuffleBotMachine:
+    STACK_COUNT = 4
+    INPUT_STACK_INDEX = 0
+    OUTPUT_STACK_INDEX = 3
+
     # Arm is fully pressed down with some amount of "pressure" applied.
-    PRESSED_DOWN = ArmStateDesc(name = "PRESSED_DOWN", direction = -1, resistance_range = range(-120, -9999, -1), max_resistance_before_stop = 0.20)
+    PRESSED_DOWN = ArmStateDesc(name = "PRESSED_DOWN", direction = -1, resistance_range = range(-120, -9999, -1), max_resistance_before_stop = 0.50)
 
     # Arm is slightly raised after being pressed down to allow cards stuck to the bottom of the top card to fall before we add
     # any more force by accelerating upward.
-    CONFIRMING_GRAB = ArmStateDesc(name = "CONFIRMING_GRAB", direction = 1, relative_position = 60)
+    CONFIRMING_GRAB = ArmStateDesc(name = "CONFIRMING_GRAB", direction = 1, relative_position = 80)
 
     # Just for testing pretend to go down but don't push hard...
     FAKE_PRESSED_DOWN = ArmStateDesc(name = "FAKE_PRESSED_DOWN", direction = -1, absolute_position = -220)
 
     # Arm is fully raised in the "release" position (a physical obstruction exists along the grabber arm path such that
     # no object should be grabbed in this state).
-    UP_RELEASED = ArmStateDesc(name = "UP_RELEASED", direction = 1, absolute_position = -10)
+    UP_RELEASED = ArmStateDesc(name = "UP_RELEASED", direction = 1, absolute_position = -2)
 
     # Arm is raised but in a position that would permit holding onto a grabbed object.
     UP_HOLDING = ArmStateDesc(name = "UP_HOLDING", direction = 1, absolute_position = -72)
@@ -72,26 +80,28 @@ class ShuffleBotMachine:
         0,
     )
 
-    STACK_GRAB_COLUMNS = (
-        0,
+    STACK_CENTER_GRAB_COLUMNS = (
+        -1,
         1,
         3,
         -1,
     )
 
-    STACK_DROP_COLUMNS = (
+    STACK_RIGHT_GRAB_COLUMNS = (
+        0,
+        2,
+        4,
+        -1,
+    )
+
+    STACK_LEFT_OR_CENTER_DROP_COLUMNS = (
         -1,
         1,
         3,
         5,
     )
 
-    STACK_RIGHT_DROP_COLUMNS = (
-        -1,
-        2,
-        4,
-        -1
-    )
+    STACK_RIGHT_DROP_COLUMNS = STACK_RIGHT_GRAB_COLUMNS
 
     INPUT_COLUMN_INDEX = 0
     OUTPUT_COLUMN_INDEX = 3
@@ -121,6 +131,9 @@ class ShuffleBotMachine:
     def calibrate_manually_as_top_right(self):
         self.gantry.calibrate_manually_as_top_right()
 
+    def reset_positions(self):
+        self.gantry.reset_positions()
+
     def move_item(self, from_stack, to_stack):
         if from_stack is None and to_stack is None:
             self._move_column_raw(None, None)
@@ -137,9 +150,8 @@ class ShuffleBotMachine:
         if from_column is not None:
             gantry.translate_arm_to_column(from_column)
         gantry.set_arm_state(self.PRESSED_DOWN)
-        sleep(0.5)
         gantry.set_arm_state(self.CONFIRMING_GRAB)
-        sleep(0.5)
+        sleep(1.5)
         gantry.set_arm_state(self.UP_HOLDING)
         if to_column is not None:
             gantry.translate_arm_to_column(to_column)
@@ -150,22 +162,23 @@ class ShuffleBotMachine:
         self.move_item(None, None)
 
     def _stack_translation_to_column_moves(self, from_stack, to_stack) -> list:
-        which_drop_columns = None
-        if from_stack == 0:
+        columns = None
+        if from_stack == 0 or to_stack == 0:
             if to_stack == 3:
                 return self._stack_translation_to_column_moves(0, 2) + self._stack_translation_to_column_moves(2, 3)
             else:
-                which_drop_columns = self.STACK_RIGHT_DROP_COLUMNS
+                columns = (self.STACK_RIGHT_GRAB_COLUMNS, self.STACK_RIGHT_DROP_COLUMNS)
         else:
-            which_drop_columns = self.STACK_DROP_COLUMNS
+            columns = (self.STACK_CENTER_GRAB_COLUMNS, self.STACK_LEFT_OR_CENTER_DROP_COLUMNS)
 
-        return [ (self.STACK_GRAB_COLUMNS[from_stack], which_drop_columns[to_stack]) ]
+        from_column = columns[0][from_stack]
+        to_column = columns[1][to_stack]
+        if from_column == -1 or to_column == -1:
+            raise Exception('Illegal move: {} to {}'.format(from_column, to_column))
+
+        return [ (from_column, to_column) ]
 
 class TwoAxisGantryMachine:
-
-    current_state = None
-    current_column = None
-    is_calibrated = False
 
     def __init__(self, arm_states, column_positions, up_down_axis: MovementAxis, left_right_axis: MovementAxis) -> None:
         self.up_down_axis = up_down_axis
@@ -173,7 +186,9 @@ class TwoAxisGantryMachine:
         self.axes = (up_down_axis, left_right_axis, )
         self.arm_states = arm_states
         self.column_positions = column_positions
-        debug_print(self.axes)
+        self.current_state = None
+        self.current_column = None
+        self.is_calibrated = False
 
     def _calibrate_individual(self, axis: MovementAxis, direction: int, speed: SpeedValue, calibrationTarget: int):
         debug_print("Calibrating {} by stepping {} up to {}".format(
@@ -190,30 +205,39 @@ class TwoAxisGantryMachine:
     def _on_until_resists(self, axis: MovementAxis, direction: int, resistance_range, max_resistance, speed: SpeedValue) -> int:
         motor = axis.motor
         startPos = motor.position
-        self._on_with_brake(motor, speed * direction)
-        motor.wait_until('running', timeout = 100)
-        lastSpeed = motor.speed
-        while motor.wait_until_not_moving(timeout = 50) is False:
-            currentPos = motor.position
-            currentSpeed = motor.speed
+        simpleMethod = True
+        if simpleMethod == True:
+            motor.on(speed * direction, brake = True, block = True)
+        else:
+            motor.on(speed * direction)
+            motor.wait_until('running', timeout = 100)
+            lastSpeed = motor.speed
+            topSpeed = -1
+            while motor.wait_until_not_moving(timeout = 100) is False:
+                currentPos = motor.position
+                currentSpeed = motor.speed
 
-            if currentPos not in axis.stable_range:
-                debug_print("No resistance detected but outside of stable range at {}!".format(currentPos))
-                break
-
-            if currentPos in resistance_range:
-                speedChange = abs(currentSpeed) - abs(lastSpeed)
-                relativeResistance = -speedChange / abs(motor.speed_sp)
-                debug_print("speed={}, speed_sp={}, speedChange={}".format(motor.speed, motor.speed_sp, speedChange))
-                debug_print("...relativeResistance={}".format(relativeResistance))
-
-                if relativeResistance > max_resistance:
-                    debug_print("Encountered resistance of {}, we're satisfied...".format(relativeResistance))
+                if currentPos not in axis.stable_range:
+                    debug_print("No resistance detected but outside of stable range at {}!".format(currentPos))
                     break
 
-            lastSpeed = currentSpeed
+                if currentSpeed > topSpeed:
+                    topSpeed = currentSpeed
 
-        motor.off(brake = True)
+                if currentPos in resistance_range:
+                    speedChange = abs(currentSpeed) - abs(lastSpeed)
+                    speedChangeFromMax = abs(topSpeed - currentSpeed)
+                    relativeResistance = -speedChange / abs(motor.speed_sp)
+                    debug_print("speed={}, speed_sp={}, speedChange={}, speedChangeFromMax={}".format(currentSpeed, motor.speed_sp, speedChange, speedChangeFromMx))
+                    debug_print("...relativeResistance={}".format(relativeResistance))
+
+                    if relativeResistance > max_resistance:
+                        debug_print("Encountered resistance of {}, we're satisfied...".format(relativeResistance))
+                        break
+
+                lastSpeed = currentSpeed
+
+        motor.off(brake = False)
         motor.wait_until_not_moving()
 
         return abs(motor.position - startPos)
@@ -228,6 +252,14 @@ class TwoAxisGantryMachine:
             motor.ramp_down_sp = axis.ramp_down_sp
 
         self.is_calibrated = True
+
+    def reset_positions(self):
+        if self.is_calibrated == False:
+            raise Exception('Must be calibrated already!')
+        
+        for axis in (self.up_down_axis, self.left_right_axis):
+            motor = axis.motor
+            self._to_position_with_brake(motor, speed = axis.init_speed_removeme, position = 0)
 
     def calibrate_automatically_wip(self):
         raise Exception("This probably will break your machine, not well tested or adjusted yet...")
@@ -308,37 +340,120 @@ class TwoAxisGantryMachine:
         motor.stop_action = Motor.STOP_ACTION_BRAKE
         motor.run_forever()
 
+class SimpleGuiPrompt:
+    def __init__(self, console: Console, button: Button, leds: Leds):
+        self.console = console
+        self.button = button
+        self.leds = leds
+
+    def prompt_number(self, prompt: str, starting_value: int) -> int:
+        value = starting_value
+        self.leds.all_off()
+        self.leds.set_color('LEFT', 'ORANGE', pct = 0.2)
+        self.leds.set_color('RIGHT', 'ORANGE', pct = 0.2)
+        while True:
+            self.console.reset_console()
+            self.console.text_at('{}{}\n'.format(prompt, value), column = 1, row = 3)
+            pressed = self._wait_for_button_press()
+            if pressed == 'up' or pressed == 'right':
+                value += 1
+            elif pressed == 'down' or pressed == 'left':
+                value -= 1
+            elif pressed == 'enter':
+                break
+        self.leds.all_off()
+        return value
+
+    def _wait_for_button_press(self):
+        button = self.button
+        pressed = None
+        while True:
+            allpressed = button.buttons_pressed
+            if bool(allpressed):
+                pressed = allpressed[0]
+                while not button.wait_for_released(pressed):
+                    pass
+                break
+        return pressed
+
+class ShuffleBotMain:
+    def main(self):
+        sound = Sound()
+        machine = ShuffleBotMachine()
+        leds = Leds()
+        button = Button()
+        console = Console()
+
+        deck_size = SimpleGuiPrompt(console, button, leds).prompt_number('Deck size: ', starting_value = 10)
+        debug_print('Running with deck size of {}'.format(deck_size))
+
+        sound.speak('Manual calibration')
+        machine.calibrate_manually_as_top_right()
+
+        sound.speak('Shuffling')
+        solver = ShuffleSolver(deck_size, machine.STACK_COUNT, machine.INPUT_COLUMN_INDEX, machine.OUTPUT_COLUMN_INDEX)
+        moves = solver.solve()
+
+        sound.speak('Here we go, {} moves'.format(len(moves)))
+
+        progress_interval = 10
+        next_progress_notice = progress_interval
+        for move_index in range(len(moves)):
+            pct_complete = move_index / len(moves) * 100
+            if pct_complete > next_progress_notice and next_progress_notice < 100:
+                sound.speak('{} percent complete'.format(next_progress_notice), play_type = Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+                next_progress_notice += progress_interval
+
+            move = moves[move_index]
+            src = move[0]
+            dst = move[1]
+            sound.speak('Move {} to {}'.format(src, dst), play_type = Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+            machine.move_item(src, dst)
+
+        sound.speak('Finished', play_type = Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+        machine.reset_positions()
+
 class FiguringStuffOutBebot:
     def __init__(self):
         self.sound = Sound()
         self.machine = ShuffleBotMachine()
 
-    def move_items(self):
+    def input_test(self):
+        leds = Leds()
+        button = Button()
+        console = Console()
+    
+        deck_size = SimpleGuiPrompt(console, button, leds).prompt_number('Deck size: ', starting_value = 40)
+        debug_print('Got {}'.format(deck_size))
+
+    def move_test(self):
         sound = self.sound
         machine = self.machine
         sound.speak('Calibrating...')
         machine.calibrate_manually_as_top_right()
         sound.speak('Ready!')
         moves = (
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 3),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
-(0, 1),
-(0, 2),
+            (1, 2),
+            (1, 3),
+            (1, 2),
+            (1, 3),
+            (1, 2),
+            (2, 3),
+            (2, 1),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (2, 1),
+            (2, 1),
+            (2, 1),
+            (1, 2),
+            (1, 2),
+            (1, 2),
+            (1, 2),
         )
         for move in moves:
             src = move[0]
@@ -409,7 +524,8 @@ def main():
         ev3dev2.Device.DEVICE_ROOT_PATH = os.path.join(fake_sys, 'arena')
 
     #fucking_around()
-    FiguringStuffOutBebot().move_items()
+    # FiguringStuffOutBebot().input_test()
+    ShuffleBotMain().main()
 
 if __name__ == '__main__':
     main()
